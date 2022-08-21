5d4b50127a972a645202827413d330e9435d89ad
josephmabusela <josephmabusela5@gmail.com>
2022-08-21 23:04:44 +0200

    comment in database.sql

--- database.sql
+++ database.sql
@@ -1,21 +1,22 @@
-CREATE DATABASE authtodolist;
-
---users table
+-- CREATE DATABASE authtodolist;
+ --users table
 
 CREATE TABLE users( user_id UUID DEFAULT uuid_generate_v4(),
                                          user_name VARCHAR(255) NOT NULL,
                                                                 user_email VARCHAR(255) NOT NULL UNIQUE,
                                                                                                  user_password VARCHAR(255) NOT NULL,
-                                                                                                                            PRIMARY KEY (user_id) ) --todos table
+                                                                                                                            PRIMARY KEY (user_id));
+
+--todos table
 
 CREATE TABLE todos( todo_id SERIAL, user_id UUID,
                                     description VARCHAR(255) NOT NULL,
                                                              PRIMARY KEY (todo_id),
-                   FOREIGN KEY (user_id) REFERENCES users(user_id) ) -- fake users data
+                   FOREIGN KEY (user_id) REFERENCES users(user_id));
 
-insert into users (user_name,
-                   user_email,
-                   user_password)
+-- fake users data
+
+insert into users (user_name, user_email, user_password)
 values ('james',
         'james@gmail.com',
         'james22');
